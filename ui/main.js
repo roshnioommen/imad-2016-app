ed3bdda3907ef75372b592b244319b69473e902d
roshnioommen <roshni.oommen@saintgits.org>
2016-10-01 16:18:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 //Change element
 var element = document.getElementById(`main-text`);
 element.innerHTML = `New Value`; 
-var img = document.getElementById(`img`);
+var img = document.getElementById(`roshni`);
 element.innerHTML = `New Value`;
 img.onclick = function () {
     img.style.marginLeft = `100px`;
